5a81014ca443f8e321943ba5c4d8f5c910c0a0da
PkHutch <pk-hutch@hotmail.com>
2026-01-14 16:33:49 -0700

    issue-13: Add halfbox entity
    There's a bug with collision.

--- python/game/entities/__init__.py
+++ python/game/entities/__init__.py
@@ -1,7 +1,9 @@
-""""
+"""
 Game entities module
 """
+from .box import Box
+from .halfbox import HalfBox
 from .player import Player
 from .entities import GameState
 
-__all__ = ['Player', 'Box', 'GameState']
+__all__ = ['Player', 'Box', 'HalfBox', 'GameState']
--- python/game/entities/entities.py
+++ python/game/entities/entities.py
@@ -2,6 +2,7 @@
 Game entities for Geometry Dash clone
 """
 from game.entities.box import Box
+from game.entities.halfbox import HalfBox
 from game.entities.player import Player
 from game.entities.entity import Entity
 
@@ -28,18 +29,18 @@ class GameState(Entity):
         if self.game_over:
             return
         
-        # Check if player is landing on any box
+        # Check if player is landing on any box or halfbox
         platform_y = None
         landed_on_box = False
         
         for box in self.boxes:
-            # Check if player is landing on top of this box
+            # Check if player is landing on top of this box/halfbox
             if box.is_player_landing_on_top(self.player):
                 platform_y = box.y  # Player can land on this box
                 landed_on_box = True
                 break
             
-            # Check if player collides with sides of box (death)
+            # Check if player collides with sides of box/halfbox (death)
             if box.collides_with_sides(self.player):
                 self.game_over = True
                 return
@@ -47,19 +48,27 @@ class GameState(Entity):
         # Update player with platform info
         self.player.update(self.ground_y, platform_y)
         
-        # Spawn boxes
+        # Spawn boxes (can spawn both Box and HalfBox)
         self.spawn_timer += 1
         if self.spawn_timer >= self.spawn_interval:
             self.spawn_timer = 0
-            # Fixed size box at ground level
-            self.boxes.append(Box(640, self.ground_y - Box.SIZE))
+            # Randomly choose between Box and HalfBox
+            import random
+            if random.random() < 0.5:
+                # Spawn regular Box
+                self.boxes.append(Box(640, self.ground_y - Box.SIZE))
+            else:
+                # Spawn HalfBox
+                self.boxes.append(HalfBox(640, self.ground_y - HalfBox.HEIGHT))
         
         # Update boxes
         for box in self.boxes:
             box.update(self.scroll_speed)
             
             # Score when box passes player
-            if box.x + Box.SIZE < self.player.x and not hasattr(box, 'scored'):
+            player_width = self.player.SIZE
+            box_width = getattr(box, 'width', Box.SIZE)
+            if box.x + box_width < self.player.x and not hasattr(box, 'scored'):
                 box.scored = True
                 self.score += 1
         
