b4488ae7fe2d9b492e320e2b3bc6ffc233845db0
sudonym-i <isickln08@gmail.com>
2026-01-19 12:01:01 -0700

    Add executables to gitignore & eliminate double main problem
    The 'make test' command was running into issues due to 2 definitions of
    'main()' when linking with the make file. I eliminate one of the main's
    to get it working, and added the executables to the gitignore.
    
    revert THIS commit if main wad

--- src/helloworld.c
+++ src/helloworld.c
@@ -5,14 +5,3 @@
 int add_numbers(int x, int y) {
     return x + y;
 }
-
-// Main function: entry point for execution
-int main() {
-
-    // Writing print statement to print hello world
-    int value = add_numbers(12,3);
-    printf("Hello World");
-    printf("Value from add: %d", value);
-
-    return 0;
-}
