f464fc8e1c0ba1b58e618924605c5d45130e75ba
PkHutch <pk-hutch@hotmail.com>
2026-02-13 14:45:17 -0700

    issue-44: Create lava
    Update tests, documentation, and make file.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h> /* Temporary printf import */
 #include "block.h"
 #include "spike.h"
+#include "lava.h"
 
 int main(void) {
     /* For now, just a placeholder */
     Block test_block = {100, 200, BLOCK_SIZE, BLOCK_SPRITE};
     Spike test_spike = {300, 400, SPIKE_SIZE, SPIKE_SPRITE};
+    Lava test_lava = {500, 600, LAVA_SIZE, LAVA_SPRITE};
 
     printf("Running main!\n");
 
