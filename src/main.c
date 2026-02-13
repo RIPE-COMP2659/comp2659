7d8fe86a12c92a150d881e34db5ad1ae2b861ce9
PkHutch <pk-hutch@hotmail.com>
2026-02-13 13:30:22 -0700

    issue-44: Create makefile, and block
    Create a RUN_MAKE which will clean the .o and .tos files, and then
    create MAKEFILE_STEEM made for STEEM. Create block files, tests, and
    include them in the MAKEFILE_STEEM.
    
    Update README with instructions.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /* Temporary printf import */
+#include "block.h"
+
+int main(void) {
+    /* For now, just a placeholder */
+    Block test_block = {100, 200, BLOCK_SIZE};
+
+    printf("Running main!\n");
+
+    return 0;
+}
