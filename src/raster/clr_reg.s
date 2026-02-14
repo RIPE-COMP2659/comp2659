39411db1758f8b807790039893f798144315360a
sudonym-i <isickln08@gmail.com>
2026-02-14 10:20:04 -0700

    issue-45: optimize for ONLY our specific use cases visibility push.

--- src/raster/clr_reg.s
+++ src/raster/clr_reg.s
@@ -22,282 +22,74 @@ width           equ             74
 
 _clear_region:  movem.l         d0-d7/a0-a6,-(sp)
 
-                movea.l         base(sp),a0
-                adda.l          col(sp),a0
-
+                movea.l         base(sp),a0     ; get base address
+                
+                ; Calculate and add row offset: row * 80 bytes
                 move.w          row(sp),d0
-                mulu.w          80,d0           ; screen width in bytes
-                adda.l          d0,a0
+                mulu.w          #80,d0          ; screen width in bytes (result is long)
+                adda.l          d0,a0           ; add row offset
+                
+                ; Calculate and add col offset: col / 8 (pixels to bytes)
+                move.w          col(sp),d0
+                lsr.w           #3,d0           ; divide by 8 (shift right 3 bits)
+                ext.l           d0              ; extend to long
+                adda.l          d0,a0           ; add col offset in bytes
 
-                move.w          length(sp),d1   ; d1 = number of rows (length)
-                move.w          width(sp),d2    ; d2 = width in bytes
+                ; Check if width == 48 pixels (6 bytes) for optimization
+                move.w          width(sp),d0
+                cmpi.w          #48,d0          ; check if exactly 48 pixels
+                bne             unoptimized
                 
-                ; Calculate how to break down width:
-                ; num_words = width / 2
-                ; num_bytes = width % 2
-                
-                move.w          d2,d3           ; d3 = width
-                lsr.w           #1,d3           ; d3 = width / 2 = num_words
-                
-                move.w          d2,d6           ; d6 = width
-                andi.w          #1,d6           ; d6 = width % 2 = num_bytes
-                
-                ; Check if we can fit in one movem (max 13 words = 26 bytes)
-                cmpi.w          #13,d3          ; can we fit all words in registers?
-                ble             fits_in_registers
-                
-large_width:
-                ; Width > 26 bytes: use multiple movem operations per row
-                lea             zeros_13,a1
-                movem   (a1),d1-d7/a1-a6  ; load 13 words of zeros
-                
-                move.w          length(sp),d0   ; d0 = row counter
-                subq.w          #1,d0           ; adjust for dbra
-                move.w          d0,-(sp)        ; save on stack
-                
-row_loop_large:  
-                move.l          a0,-(sp)        ; save row start
-                move.w          80(sp),d0       ; reload width (adjusted for stack)
-                
-chunk_loop:     
-                cmpi.w          #26,d0          ; can write full 26-byte chunk?
-                blt             handle_remainder
-                
-                movem   d1-d7/a1-a6,(a0)  ; write 26 bytes
-                lea             26(a0),a0       
-                subi.w          #26,d0          
-                bra             chunk_loop
-                
-handle_remainder:
-                bra             rem_check_word
-                
-rem_do_word:
-                move.w          d1,(a0)+        
-                subi.w          #2,d0
-                
-rem_check_word:
-                cmpi.w          #2,d0
-                bge             rem_do_word
-                
-rem_check_byte:
-                tst.w           d0
-                beq             row_done_large
-                move.b          d1,(a0)+        
-                
-row_done_large:
-                movea.l         (sp)+,a0        ; restore row start
-                adda.w          #80,a0          ; next row
-                move.w          (sp),d0         ; reload counter
-                dbra            d0,row_loop_large
-                
-                addq.w          #2,sp           ; pop counter
-                bra             exit_clear
-                
-fits_in_registers:
-                ; Width <= 26 bytes: use single movem per row with exact register count
-                lea             zeros_13,a1
-                
-                ; Load zeros into required number of registers
-                ; We'll load all 13 and use only what we need in movem
-                movem   (a1),d1-d7/a1-a6
-                
-                ; Calculate movem register mask based on num_words in d3
-                ; d3 = 0: no movem needed
-                ; d3 = 1-7: use d1-dX
-                ; d3 = 8-13: use d1-d7/a1-aX
-                
-                move.w          length(sp),d0   ; d0 = row counter  
-                subq.w          #1,d0           ; adjust for dbra
-                
-                tst.w           d3              ; any words to write?
-                beq             no_movem_needed
-                
-                ; Determine which movem instruction to use
-                cmpi.w          #1,d3
-                beq             use_1_reg
-                cmpi.w          #2,d3
-                beq             use_2_reg
-                cmpi.w          #3,d3
-                beq             use_3_reg
-                cmpi.w          #4,d3
-                beq             use_4_reg
-                cmpi.w          #5,d3
-                beq             use_5_reg
-                cmpi.w          #6,d3
-                beq             use_6_reg
-                cmpi.w          #7,d3
-                beq             use_7_reg
-                cmpi.w          #8,d3
-                beq             use_8_reg
-                cmpi.w          #9,d3
-                beq             use_9_reg
-                cmpi.w          #10,d3
-                beq             use_10_reg
-                cmpi.w          #11,d3
-                beq             use_11_reg
-                cmpi.w          #12,d3
-                beq             use_12_reg
-                bra             use_13_reg      ; d3 = 13
-                
-use_1_reg:
-row_loop_1:     move.w          d1,(a0)
-                lea             2(a0),a0
-                tst.w           d6
-                beq             row_done_1
-                move.b          d1,(a0)+
-row_done_1:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_1
-                bra             exit_clear
-                
-use_2_reg:
-row_loop_2:     movem   d1-d2,(a0)
-                lea             4(a0),a0
-                tst.w           d6
-                beq             row_done_2
-                move.b          d1,(a0)+
-row_done_2:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_2
-                bra             exit_clear
-                
-use_3_reg:
-row_loop_3:     movem   d1-d3,(a0)
-                lea             6(a0),a0
-                tst.w           d6
-                beq             row_done_3
-                move.b          d1,(a0)+
-row_done_3:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_3
-                bra             exit_clear
-                
-use_4_reg:
-row_loop_4:     movem   d1-d4,(a0)
-                lea             8(a0),a0
-                tst.w           d6
-                beq             row_done_4
-                move.b          d1,(a0)+
-row_done_4:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_4
-                bra             exit_clear
-                
-use_5_reg:
-row_loop_5:     movem   d1-d5,(a0)
-                lea             10(a0),a0
-                tst.w           d6
-                beq             row_done_5
-                move.b          d1,(a0)+
-row_done_5:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_5
-                bra             exit_clear
-                
-use_6_reg:
-row_loop_6:     movem   d1-d6,(a0)
-                lea             12(a0),a0
-                tst.w           d6
-                beq             row_done_6
-                move.b          d1,(a0)+
-row_done_6:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_6
-                bra             exit_clear
-                
-use_7_reg:
-row_loop_7:     movem   d1-d7,(a0)
-                lea             14(a0),a0
-                tst.w           d6
-                beq             row_done_7
-                move.b          d1,(a0)+
-row_done_7:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_7
-                bra             exit_clear
-                
-use_8_reg:
-row_loop_8:     movem   d1-d7/a1,(a0)
-                lea             16(a0),a0
-                tst.w           d6
-                beq             row_done_8
-                move.b          d1,(a0)+
-row_done_8:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_8
-                bra             exit_clear
-                
-use_9_reg:
-row_loop_9:     movem   d1-d7/a1-a2,(a0)
-                lea             18(a0),a0
-                tst.w           d6
-                beq             row_done_9
-                move.b          d1,(a0)+
-row_done_9:     suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_9
-                bra             exit_clear
-                
-use_10_reg:
-row_loop_10:    movem   d1-d7/a1-a3,(a0)
-                lea             20(a0),a0
-                tst.w           d6
-                beq             row_done_10
-                move.b          d1,(a0)+
-row_done_10:    suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_10
-                bra             exit_clear
-                
-use_11_reg:
-row_loop_11:    movem   d1-d7/a1-a4,(a0)
-                lea             22(a0),a0
-                tst.w           d6
-                beq             row_done_11
-                move.b          d1,(a0)+
-row_done_11:    suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_11
-                bra             exit_clear
-                
-use_12_reg:
-row_loop_12:    movem   d1-d7/a1-a5,(a0)
-                lea             24(a0),a0
-                tst.w           d6
-                beq             row_done_12
-                move.b          d1,(a0)+
-row_done_12:    suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_12
-                bra             exit_clear
-                
-use_13_reg:
-row_loop_13:    movem   d1-d7/a1-a6,(a0)
-                lea             26(a0),a0
-                tst.w           d6
-                beq             row_done_13
-                move.b          d1,(a0)+
-row_done_13:    suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_13
-                bra             exit_clear
+                ; Ensure word alignment for movem.w (address must be even)
+                move.l          a0,d1
+                btst            #0,d1           ; test if address is odd
+                bne             unoptimized     ; if odd, use unoptimized version
+                bra             opt_48x48
 
-no_movem_needed:
-                ; Only byte to write
-row_loop_tiny:  
-                tst.w           d6
-                beq             row_done_tiny
-                move.b          d1,(a0)+
+opt_48x48:      
+                ; Optimized for 48x48 sprites (6 bytes = 3 words wide)
+                ; Uses movem.w to clear 3 words at once
                 
-row_done_tiny:
-                suba.l          d2,a0
-                adda.w          #80,a0
-                dbra            d0,row_loop_tiny
-                bra             exit_clear
-
-exit_clear:
+                moveq           #0,d1           ; clear registers to zero
+                moveq           #0,d2
+                moveq           #0,d3
+                
+                move.w          length(sp),d7   ; get height (number of rows in pixels)
+                subq.w          #1,d7           ; adjust for dbra
+                
+row_loop_48:    movem.w         d1-d3,(a0)      ; write 3 words (6 bytes)
+                adda.w          #80,a0          ; move to next row (80 bytes per row)
+                dbra            d7,row_loop_48
+                
                 movem.l         (sp)+,d0-d7/a0-a6
                 rts
 
-
-zeros_13:            ds.w            13
+unoptimized:    
+                ; Generic clear region - calculate bytes needed for col through col+width-1
+                ; Formula: ceiling((col+width)/8) - floor(col/8)
+                
+                move.w          col(sp),d6      ; get col in pixels
+                add.w           width(sp),d6    ; d6 = col + width
+                addq.w          #7,d6           ; d6 = col + width + 7 (for ceiling division)
+                lsr.w           #3,d6           ; d6 = (col + width + 7) / 8 = ceiling((col+width)/8)
+                
+                move.w          col(sp),d5      ; get col in pixels  
+                lsr.w           #3,d5           ; d5 = col / 8 = floor(col/8)
+                
+                sub.w           d5,d6           ; d6 = ceiling - floor = num_bytes
+                subq.w          #1,d6           ; adjust for dbra
+                
+                move.w          length(sp),d7   ; outer loop: rows (in pixels)
+                subq.w          #1,d7           ; adjust for dbra
+                
+row_loop:       movea.l         a0,a1           ; save row start position
+                move.w          d6,d5           ; restore column counter
+                
+col_loop:       clr.b           (a1)+           ; clear one byte, advance
+                dbra            d5,col_loop
+                
+                adda.w          #80,a0          ; move to next row
+                dbra            d7,row_loop
+                
+                movem.l         (sp)+,d0-d7/a0-a6
+                rts
--- src/raster/test.c
+++ src/raster/test.c
@@ -7,71 +7,20 @@
 void disable_cursor();
 void fill_screen(UINT32 *base, char pattern);
 
-int main() {
+int main()
+{
     UINT32 *base = (UINT32 *)Physbase();
-    
-    disable_cursor(); /* hide blinking text cursor */
-    
-    /* Fill screen with all black to start */
+
+    /* disable_cursor(); hide blinking text cursor */
+
+    /* Fill screen with all black to start
     fill_screen(base, -1);
-    Cnecin(); /* wait for key press */
-    
+    Cnecin();  wait for key press */
+
     /* Test 1: Small region - 4 bytes wide (1 long register), 10 rows */
     /* Clears from row 10, col 5 bytes, height 10 rows, width 4 bytes */
-    clear_region(base, 10, 5, 10, 4);
+    clear_region(base, 0, 0, 48, 48);
     Cnecin();
-    
-    /* Refill screen */
-    fill_screen(base, -1);
-    
-    /* Test 2: Medium region - 20 bytes wide (5 long registers), 15 rows */
-    /* Clears from row 50, col 10 bytes, height 15 rows, width 20 bytes */
-    clear_region(base, 50, 10, 15, 20);
-    Cnecin();
-    
-    /* Refill screen */
-    fill_screen(base, -1);
-    
-    /* Test 3: Larger region - 52 bytes wide (13 long registers), 20 rows */
-    /* Clears from row 100, col 0 bytes, height 20 rows, width 52 bytes */
-    clear_region(base, 100, 0, 20, 52);
-    Cnecin();
-    
-    /* Refill screen */
-    fill_screen(base, -1);
-    
-    /* Test 4: Very large region - 100 bytes wide (requires multiple movem), 30 rows */
-    /* Clears from row 200, col 0 bytes, height 30 rows, width 100 bytes */
-    clear_region(base, 200, 0, 30, 100);
-    Cnecin();
-    
-    /* Refill screen */
-    fill_screen(base, -1);
-    
-    /* Test 5: Region with odd width - 13 bytes (3 longs + 1 byte), 5 rows */
-    /* Tests partial byte handling */
-    clear_region(base, 300, 10, 5, 13);
-    Cnecin();
-    
-    /* Refill screen */
-    fill_screen(base, -1);
-    
-    /* Test 6: Region with word alignment - 18 bytes (4 longs + 1 word), 8 rows */
-    /* Tests word remainder handling */
-    clear_region(base, 350, 20, 8, 18);
-    Cnecin();
-    
-    /* Refill screen */
-    fill_screen(base, -1);
-    
-    /* Test 7: Tiny region - 3 bytes (no full longs), 5 rows */
-    /* Tests no_movem_needed path */
-    clear_region(base, 380, 30, 5, 3);
-    Cnecin();
-    
-    /* Clear screen at the end */
-    fill_screen(base, 0);
-    
     return 0;
 }
 
@@ -85,7 +34,7 @@ void fill_screen(UINT32 *base, char pattern)
 {
     register int i = 0;
     register UINT32 *loc = base;
-    
+
     while (i++ < BYTES_PER_SCREEN / 4)
         *(loc++) = pattern;
 }
