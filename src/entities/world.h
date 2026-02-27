61f6a043d41faff17c19756778a19919ff9d8b43
PkHutch <pk-hutch@hotmail.com>
2026-02-27 13:09:15 -0700

    issue-44: Add world camera indexing
    This is the optimization for rendering so we know which indexes are
    needed in order to render without looping through every object.

--- src/entities/world.h
+++ src/entities/world.h
@@ -8,10 +8,20 @@ typedef struct {
   Geo geo;
   unsigned int ground_y;
   Camera camera;
+  unsigned int cam_min_bi;
+  unsigned int cam_max_bi;
+  unsigned int cam_min_si; /* spike index */
+  unsigned int cam_max_si; /* spike index */
+  unsigned int cam_min_li; /* lava index */
+  unsigned int cam_max_li; /* lava index */
 } World;
 
 World create_world(Level *levels, Geo geo, unsigned int ground_y);
 World get_world(void);
+void world_update_camera(World *world, unsigned int level_index);
+void world_update_camera_bi(World *world, unsigned int level_index);
+void world_update_camera_si(World *world, unsigned int level_index);
+void world_update_camera_li(World *world, unsigned int level_index);
 void world_collision_geo_block(World *world, Block *block);
 void world_collision_geo_spike(World *world, Spike *spike);
 void world_collision_geo_lava(World *world, Lava *lava);
