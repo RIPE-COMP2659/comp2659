bdd1f48df22b2035483e0a82ca691820edcc8870
PkHutch <pk-hutch@hotmail.com>
2026-03-01 19:22:15 -0700

    issue-55: Add missing newlines from .s files
    Namely plt_lin and plt_vrt, otherwise we had compilation issues.

--- src/raster/plt_vrt.s
+++ src/raster/plt_vrt.s
@@ -1,5 +1,5 @@
 ;
-; PURPOSE: Plot a hoizontal line on the screen. The vertical line is specified by the topmost pixel of the line and the length of the line.
+; PURPOSE: Plot a vertical line on the screen. The vertical line is specified by the topmost pixel of the line and the length of the line.
 ;
 ; INPUT: Address(UINT32*): to the start of the screen
 ;        Position(row,col): the coordinates of the topmost pixel of the vertical line
