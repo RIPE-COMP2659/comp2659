084d3a3d99b276882f83ff8804f678094ec71db8
PkHutch <pk-hutch@hotmail.com>
2026-03-01 23:30:22 -0700

    issue-72: Partially clean up object creation and sprite ownership (#84)
    * Add create_block and documentation
    * Update camera documentation
    * Update geo partially
    The tests still need updating, same with the .c file, but the .h file
    has been cleaned up properly.
    * Update block back to 4 spaces

--- src/entities/level.c
+++ src/entities/level.c
@@ -1,5 +1,13 @@
 #include "level.h"
 
+#define L1_BLOCKS_SIZE 12
+#define L1_SPIKES_SIZE 2
+#define L1_LAVA_SIZE 5
+#define L2_BLOCKS_SIZE 2
+#define L2_SPIKES_SIZE 1
+#define L2_LAVA_SIZE 1
+#define NUM_LEVELS 2
+
 Level create_level(
     Block* blocks,
     Spike* spikes,
@@ -23,74 +31,68 @@ Level create_level(
 }
 
 Level get_level1(void) {
-    static Block level_blocks[12] = {
-        {288, 64, BLOCK_SIZE, BLOCK_SPRITE},
-        {352, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {416, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {480, 64, BLOCK_SIZE, BLOCK_SPRITE},
-        {608, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {640, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {672, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {704, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {736, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {768, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {800, 96, BLOCK_SIZE, BLOCK_SPRITE},
-        {832, 96, BLOCK_SIZE, BLOCK_SPRITE}
-    };
-    static Spike level_spikes[2] = {
+    static Block level_blocks[L1_BLOCKS_SIZE];
+    static Spike level_spikes[L1_SPIKES_SIZE] = {
         {256, 64, SPIKE_SIZE, SPIKE_SPRITE},
         {512, 64, SPIKE_SIZE, SPIKE_SPRITE}
     };
-    static Lava level_lava[5] = {
+    static Lava level_lava[L1_LAVA_SIZE] = {
         {320, 32, LAVA_SIZE, LAVA_SPRITE},
         {352, 32, LAVA_SIZE, LAVA_SPRITE},
         {384, 32, LAVA_SIZE, LAVA_SPRITE},
         {416, 32, LAVA_SIZE, LAVA_SPRITE},
         {448, 32, LAVA_SIZE, LAVA_SPRITE}
     };
-    unsigned int blocks_size = (sizeof(level_blocks) / sizeof(level_blocks[0]));
-    unsigned int spikes_size = (sizeof(level_spikes) / sizeof(level_spikes[0]));
-    unsigned int lava_size = (sizeof(level_lava) / sizeof(level_lava[0]));
+
+    level_blocks[0] = create_block(288, 64);
+    level_blocks[1] = create_block(352, 96);
+    level_blocks[2] = create_block(416, 96);
+    level_blocks[3] = create_block(480, 64);
+    level_blocks[4] = create_block(608, 96);
+    level_blocks[5] = create_block(640, 96);
+    level_blocks[6] = create_block(672, 96);
+    level_blocks[7] = create_block(704, 96);
+    level_blocks[8] = create_block(736, 96);
+    level_blocks[9] = create_block(768, 96);
+    level_blocks[10] = create_block(800, 96);
+    level_blocks[11] = create_block(832, 96);
 
     return create_level(
         level_blocks,
         level_spikes,
         level_lava,
-        blocks_size,
-        spikes_size,
-        lava_size,
+        L1_BLOCKS_SIZE,
+        L1_SPIKES_SIZE,
+        L1_LAVA_SIZE,
         1000
     );
 }
 
 Level get_level2(void) {
-    static Block level_blocks[2] = {
-        {0, 0, BLOCK_SIZE, BLOCK_SPRITE},
-        {100, 200, BLOCK_SIZE, BLOCK_SPRITE}
-    };
-    static Spike level_spikes[1] = {
+    static Block level_blocks[L2_BLOCKS_SIZE];
+    static Spike level_spikes[L2_SPIKES_SIZE] = {
         {300, 400, SPIKE_SIZE, SPIKE_SPRITE}
     };
-    static Lava level_lava[1] = {
+    static Lava level_lava[L2_LAVA_SIZE] = {
         {500, 600, LAVA_SIZE, LAVA_SPRITE}
     };
-    unsigned int blocks_size = (sizeof(level_blocks) / sizeof(level_blocks[0])) - 1;
-    unsigned int spikes_size = (sizeof(level_spikes) / sizeof(level_spikes[0])) - 1;
-    unsigned int lava_size = (sizeof(level_lava) / sizeof(level_lava[0])) - 1;
+
+    level_blocks[0] = create_block(100, 100);
+    level_blocks[1] = create_block(200, 200);
 
     return create_level(
         level_blocks,
         level_spikes,
         level_lava,
-        blocks_size,
-        spikes_size,
-        lava_size,
+        L2_BLOCKS_SIZE,
+        L2_SPIKES_SIZE,
+        L2_LAVA_SIZE,
         1000
     );
 }
 
 Level* get_levels(void) {
-    static Level levels[2];
+    static Level levels[NUM_LEVELS];
 
     levels[0] = get_level1();
     levels[1] = get_level2();
