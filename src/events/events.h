30bc8836e1c8a87a64e59355a533b480c9b92e0a
PkHutch <pk-hutch@hotmail.com>
2026-03-05 20:54:45 -0700

    Update events to only include other headers

--- src/events/events.h
+++ src/events/events.h
@@ -5,21 +5,6 @@
  *          Includes all three event submodules and defines
  *          shared event return codes.
  */
-
-#ifndef EVENTS_H
-#define EVENTS_H
-
-/* Event return codes used by conditional event handlers */
-#define EVENT_ERROR -1
-#define EVENT_NONE 0
-#define EVENT_DEATH 1
-#define EVENT_LANDED 2
-#define EVENT_LEVEL_DONE 3
-#define EVENT_OFF_PLATFORM 4
-
 #include "async.h"
 #include "sync.h"
 #include "cond.h"
-#include "sync.h"
-
-#endif
