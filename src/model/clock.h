e19e62f35c657812256995c521aa38cfe1ea695b
PkHutch <pk-hutch@hotmail.com>
2026-03-18 15:27:45 -0600

    Remove clock ifndef, should only have one import

--- src/model/clock.h
+++ src/model/clock.h
@@ -1,11 +1,6 @@
-#ifndef CLOCK_H
-#define CLOCK_H
-
 #include <osbind.h>
 
 /* Return the system timer longword (auto-incremented 70 times/sec).
  * Uses privileged mode briefly to read protected memory at 0x462.
  */
 unsigned long get_time(void);
-
-#endif /* CLOCK_H */
