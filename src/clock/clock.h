f36a08cb979dea6094687b3576e5d2aed9e40b6b
PkHutch <pk-hutch@hotmail.com>
2026-03-18 15:26:56 -0600

    Remove osbind.h from clock.c, put in .h
    Following our silly convention of imports live in the header.

--- src/clock/clock.h
+++ src/clock/clock.h
@@ -1,5 +1,7 @@
 #include <osbind.h>
 
+#include <osbind.h>
+
 /* Return the system timer longword (auto-incremented 70 times/sec).
  * Uses privileged mode briefly to read protected memory at 0x462.
  */
