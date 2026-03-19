22168924e216bb2c9291b17f5382416eebabe839
PkHutch <pk-hutch@hotmail.com>
2026-03-18 21:47:15 -0600

    Update tasync for new jumping logic

--- src/tests/events/tasync.c
+++ src/tests/events/tasync.c
@@ -5,8 +5,12 @@ Model test_model;
 
 void setUp(void) {
     test_model = get_model();
-    test_model.world.geo.dy = -10; /* Set to a non-jump value */
-    test_model.world.geo.is_landed = TRUE; /* Ensure geo can jump */
+    /* Place Geo on the ground so geo_update_landed keeps is_landed true. */
+    test_model.world.geo.ground_y = test_model.world.ground_y;
+    test_model.world.geo.y = test_model.world.geo.ground_y + test_model.world.geo.size;
+    test_model.world.geo.y_scaled = test_model.world.geo.y << GEO_PHYSICS_SHIFT;
+    test_model.world.geo.dy = 0;
+    test_model.world.geo.is_landed = TRUE;
 }
 
 void tearDown(void) {
@@ -16,6 +20,7 @@ void test_on_jump_request_calls_geo_jump(void) {
     signed int expected_jump_dy = GEO_JUMP_DY_SCALED;
 
     on_jump_request(&test_model);
+    model_update(&test_model); /* Update the model to process the jump buffer */
 
     TEST_ASSERT_EQUAL_INT(expected_jump_dy, test_model.world.geo.dy);
 }
