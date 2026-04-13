67966604ca14d0ea6a9a7e0509168332f9a2c782
Isaac K. <isickln08@gmail.com>
2026-04-13 15:14:57 -0600

    Refactor input handling and improve game logic

--- src/dash.c
+++ src/dash.c
@@ -7,8 +7,8 @@
 #include "psg/effects.h"
 #include "psg/music.h"
 
-#define JUMP SC_SPACE
-#define QUIT SC_Q
+#define JUMP 32
+#define QUIT 113
 
 /* TODO: Death is a little bit out of sync */
 int main_game(void)
@@ -16,14 +16,11 @@ int main_game(void)
     Model model = get_model();
     UINT8 quit = FALSE;
     UINT8 game_won = FALSE;
-    UINT8 init;
     signed int died_this_frame;
-    unsigned int input;
     signed int current_event;
     unsigned long timeThen, timeNow, timeElapsed;
 
-    init = splash_screen(); /* Prints splash screen and awaits user input */
-    init_input(); /* Initializes input handling and installs IKBD ISR */
+    UINT8 init = splash_screen(); /* Prints splash screen and awaits user input */
 
     if (init == 0)
     {
@@ -31,9 +28,6 @@ int main_game(void)
     }
     else
     {
-        UINT8 jumping = FALSE;
-        SCANCODE sc;
-
         /* Initialize render buffers */
         init_render_buffers();
         /* Initial render */
@@ -48,71 +42,62 @@ int main_game(void)
         timeThen = get_time();
         while (quit != TRUE && game_won != TRUE)
         {
-            /* Silly-while consumes both make and breaks properly
-               otherwise jumping logic becomes far more verbose */
-            /* Not ideal, but necessary for time constraints */
-            while ((sc = get_input()) != 0) {
-                switch (sc) {
-                    case JUMP:
-                        jumping = TRUE;
-                        break;
-                    case JUMP | BREAK_BIT:
-                        jumping = FALSE;
-                        break;
-                    case QUIT:
-                        quit = TRUE;
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            if (jumping == TRUE) {
+            switch (get_input())
+            {
+            case JUMP:
                 on_jump_request(&model);
+                break;
+            case QUIT:
+                quit = TRUE;
+                break;
+            default:
+                break;
             }
 
             timeNow = get_time();
             timeElapsed = timeNow - timeThen;
-            timeThen = timeNow; /* update here, before on_clock_tick's death wait */
 
             /* NOTE: This keeps the game from crashing, no clue why, something in music timing */
-            if (timeElapsed == 0)
+            if (timeElapsed != 0)
             {
-                timeElapsed = 1;
+
+                /* Update music with elapsed VBL ticks */
+                update_music(timeElapsed);
+
+                /* Step gameplay only on odd clock ticks. */
+                if ((timeNow & 1) != 0)
+                {
+                    died_this_frame = on_clock_tick(&model);
+
+                    if (died_this_frame == TRUE)
+                    {
+                        clear_render_buffers();
+                        start_music(SONG_MEGALO);
+                    }
+
+                    current_event = check_level_complete(&model);
+                    if (current_event == EVENT_LEVEL_DONE)
+                    {
+                        stop_music();
+                        play_level_complete_effect();
+                        game_won = TRUE;
+                    }
+
+                    /* Initial render */
+                    render(&model, 0);
+                }
+
+                /* Advance frame baseline to avoid reusing old elapsed time. */
+                timeThen = timeNow;
             }
-
-            /* Update music with elapsed VBL ticks */
-            update_music(timeElapsed);
-
-            died_this_frame = on_clock_tick(&model);
-
-            if (died_this_frame == TRUE)
-            {
-                clear_render_buffers();
-                start_music(SONG_MEGALO);
-            }
-
-            current_event = check_level_complete(&model);
-            if (current_event == EVENT_LEVEL_DONE)
-            {
-                stop_music();
-                play_level_complete_effect();
-                game_won = TRUE;
-            }
-
-            render(&model, 0);
         }
 
         /* Renable the keyboard */
         toggle_keyboard_sound();
+
+        /* Clean up music */
         stop_music();
-        restore_input();
-        shutdown_render_buffers();
-
-        return 0;
     }
-
-    restore_input(); /* Restore original IKBD vector */
 
     return 0;
 }
